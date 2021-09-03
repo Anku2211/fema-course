5fa85423dde80a87f6f0356df377ba7af8c1a984
Ankita Yadav <ankita@plantyouridea.com>
2021-09-03 11:19:15 +0530

    Added adapt-test component

---
+++ src/components/adapt-test/libraries/handlebarHelpers.js
@@ -0,0 +1,6 @@
+// define(["handlebars", "core/js/adapt"], function (Handlebars, Adapt) {
+//   helper = Handlebars.registerHelper("reverseArray", function (array) {
+//     return array.reverse();
+//   });
+//   return helper;
+// });
